38c7617f55a2a021ee2b81bd0582b27aa1e12dd2
Miguel Bayon <miguel.bayon@iessanandres.com>
2018-10-10 12:29:01 +0200

    Ahora aparece también el lugar de residencia

--- Prueba.java
+++ Prueba.java
@@ -2,5 +2,6 @@ public class Prueba{
     public static void main(String args[]){
         System.out.println("Samuel");
         System.out.println("León");
+        System.out.println("Benavides");
     }
 }
